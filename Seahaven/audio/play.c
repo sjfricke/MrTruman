4a528a1f8c4f81e0028168e8800fee162772c475
Max <mbstrange@wisc.edu>
2017-11-10 23:45:42 +0000

    programmatic capture and playback

--- Seahaven/audio/play.c
+++ Seahaven/audio/play.c
@@ -1,123 +1,125 @@
 /*
-
-This example reads standard from input and writes
-to the default PCM device for 5 seconds of data.
-
-
-We should see that the program is not useful unless the 
-input is redirected to something other than a console. 
-Try running it with the device /dev/urandom, which 
-produces random data, like this:
-
-./example3 < /dev/urandom
-
-*/
-
-/* Use the newer ALSA API */
-#define ALSA_PCM_NEW_HW_PARAMS_API
+ * Simple sound playback using ALSA API and libasound.
+ *
+ * Compile:
+ * $ cc -o play sound_playback.c -lasound
+ * 
+ * Usage:
+ * $ ./play <sample_rate> <channels> <seconds> < <file>
+ * 
+ * Examples:
+ * $ ./play 44100 2 5 < /dev/urandom
+ * $ ./play 22050 1 8 < /path/to/file.wav
+ *
+ * Copyright (C) 2009 Alessandro Ghedini <alessandro@ghedini.me>
+ * --------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Alessandro Ghedini wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we
+ * meet some day, and you think this stuff is worth it, you can
+ * buy me a beer in return.
+ * --------------------------------------------------------------
+ */
 
 #include <alsa/asoundlib.h>
+#include <stdio.h>
 
-int main() {
-  long loops;
-  int rc;
-  int size;
-  snd_pcm_t *handle;
-  snd_pcm_hw_params_t *params;
-  unsigned int val;
-  int dir;
-  snd_pcm_uframes_t frames;
-  char *buffer;
+#define PCM_DEVICE "default"
 
-  /* Open PCM device for playback. */
-  rc = snd_pcm_open(&handle, "default",
-                    SND_PCM_STREAM_PLAYBACK, 0);
-  if (rc < 0) {
-    fprintf(stderr,
-            "unable to open pcm device: %s\n",
-            snd_strerror(rc));
-    exit(1);
-  }
+int main(int argc, char **argv) {
+	unsigned int pcm, tmp, dir;
+	int rate, channels, seconds;
+	snd_pcm_t *pcm_handle;
+	snd_pcm_hw_params_t *params;
+	snd_pcm_uframes_t frames;
+	char *buff;
+	int buff_size, loops;
 
-  /* Allocate a hardware parameters object. */
-  snd_pcm_hw_params_alloca(&params);
+	if (argc < 4) {
+		printf("Usage: %s <sample_rate> <channels> <seconds>\n",
+								argv[0]);
+		return -1;
+	}
 
-  /* Fill it in with default values. */
-  snd_pcm_hw_params_any(handle, params);
+	rate 	 = atoi(argv[1]);
+	channels = atoi(argv[2]);
+	seconds  = atoi(argv[3]);
 
-  /* Set the desired hardware parameters. */
+	/* Open the PCM device in playback mode */
+	if (pcm = snd_pcm_open(&pcm_handle, "plughw:0,1",
+					SND_PCM_STREAM_PLAYBACK, 0) < 0) 
+		printf("ERROR: Can't open \"%s\" PCM device. %s\n",
+					PCM_DEVICE, snd_strerror(pcm));
 
-  /* Interleaved mode */
-  snd_pcm_hw_params_set_access(handle, params,
-                      SND_PCM_ACCESS_RW_INTERLEAVED);
+	/* Allocate parameters object and fill it with default values*/
+	snd_pcm_hw_params_alloca(&params);
 
-  /* Signed 16-bit little-endian format */
-  snd_pcm_hw_params_set_format(handle, params,
-                              SND_PCM_FORMAT_S16_LE);
+	snd_pcm_hw_params_any(pcm_handle, params);
 
-  /* Two channels (stereo) */
-  snd_pcm_hw_params_set_channels(handle, params, 2);
+	/* Set parameters */
+	if (pcm = snd_pcm_hw_params_set_access(pcm_handle, params,
+					SND_PCM_ACCESS_RW_INTERLEAVED) < 0) 
+		printf("ERROR: Can't set interleaved mode. %s\n", snd_strerror(pcm));
 
-  /* 44100 bits/second sampling rate (CD quality) */
-  val = 44100;
-  snd_pcm_hw_params_set_rate_near(handle, params,
-                                  &val, &dir);
+	if (pcm = snd_pcm_hw_params_set_format(pcm_handle, params,
+						SND_PCM_FORMAT_S16_LE) < 0) 
+		printf("ERROR: Can't set format. %s\n", snd_strerror(pcm));
 
-  /* Set period size to 32 frames. */
-  frames = 32;
-  snd_pcm_hw_params_set_period_size_near(handle,
-                              params, &frames, &dir);
+	if (pcm = snd_pcm_hw_params_set_channels(pcm_handle, params, channels) < 0) 
+		printf("ERROR: Can't set channels number. %s\n", snd_strerror(pcm));
 
-  /* Write the parameters to the driver */
-  rc = snd_pcm_hw_params(handle, params);
-  if (rc < 0) {
-    fprintf(stderr,
-            "unable to set hw parameters: %s\n",
-            snd_strerror(rc));
-    exit(1);
-  }
+	if (pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0) < 0) 
+		printf("ERROR: Can't set rate. %s\n", snd_strerror(pcm));
 
-  /* Use a buffer large enough to hold one period */
-  snd_pcm_hw_params_get_period_size(params, &frames,
-                                    &dir);
-  size = frames * 4; /* 2 bytes/sample, 2 channels */
-  buffer = (char *) malloc(size);
+	/* Write parameters */
+	if (pcm = snd_pcm_hw_params(pcm_handle, params) < 0)
+		printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(pcm));
 
-  /* We want to loop for 5 seconds */
-  snd_pcm_hw_params_get_period_time(params,
-                                    &val, &dir);
-  /* 5 seconds in microseconds divided by
-   * period time */
-  loops = 5000000 / val;
+	/* Resume information */
+	printf("PCM name: '%s'\n", snd_pcm_name(pcm_handle));
 
-  while (loops > 0) {
-    loops--;
-    rc = read(0, buffer, size);
-    if (rc == 0) {
-      fprintf(stderr, "end of file on input\n");
-      break;
-    } else if (rc != size) {
-      fprintf(stderr,
-              "short read: read %d bytes\n", rc);
-    }
-    rc = snd_pcm_writei(handle, buffer, frames);
-    if (rc == -EPIPE) {
-      /* EPIPE means underrun */
-      fprintf(stderr, "underrun occurred\n");
-      snd_pcm_prepare(handle);
-    } else if (rc < 0) {
-      fprintf(stderr,
-              "error from writei: %s\n",
-              snd_strerror(rc));
-    }  else if (rc != (int)frames) {
-      fprintf(stderr,
-              "short write, write %d frames\n", rc);
-    }
-  }
+	printf("PCM state: %s\n", snd_pcm_state_name(snd_pcm_state(pcm_handle)));
 
-  snd_pcm_drain(handle);
-  snd_pcm_close(handle);
-  free(buffer);
+	snd_pcm_hw_params_get_channels(params, &tmp);
+	printf("channels: %i ", tmp);
 
-  return 0;
+	if (tmp == 1)
+		printf("(mono)\n");
+	else if (tmp == 2)
+		printf("(stereo)\n");
+
+	snd_pcm_hw_params_get_rate(params, &tmp, 0);
+	printf("rate: %d bps\n", tmp);
+
+	printf("seconds: %d\n", seconds);	
+
+	/* Allocate buffer to hold single period */
+	snd_pcm_hw_params_get_period_size(params, &frames, 0);
+
+	buff_size = frames * channels * 2 /* 2 -> sample size */;
+	buff = (char *) malloc(buff_size);
+
+	snd_pcm_hw_params_get_period_time(params, &tmp, NULL);
+
+	for (loops = (seconds * 1000000) / tmp; loops > 0; loops--) {
+
+		if (pcm = read(0, buff, buff_size) == 0) {
+			printf("Early end of file.\n");
+			return 0;
+		}
+
+		if (pcm = snd_pcm_writei(pcm_handle, buff, frames) == -EPIPE) {
+			printf("XRUN.\n");
+			snd_pcm_prepare(pcm_handle);
+		} else if (pcm < 0) {
+			printf("ERROR. Can't write to PCM device. %s\n", snd_strerror(pcm));
+		}
+
+	}
+
+	snd_pcm_drain(pcm_handle);
+	snd_pcm_close(pcm_handle);
+	free(buff);
+
+	return 0;
 }
