0f83ca1eff9f2363db86e100e261cf6f5ba05325
sjfricke <sjfricke@wisc.edu>
2017-11-13 21:54:04 +0000

    added voice control example

--- Seahaven/audio/commands.c
+++ Seahaven/audio/commands.c
@@ -1,27 +1,43 @@
 #include "commands.h"
 
-char* _red = "red";
+static char* c_lights = "LIGHT";
+
+static char* c_picture = "PICTURE";
+static char* c_take = "TAKE";
+/*
 char* _green = "green";
 char* _blue = "blue";
 char* _yellow = "yellow";
 char* testType = "0";
 char* testValue = "0";
+*/
 
 int commandDetect(char const* voice_str)
 {
+  	
   char* detect;
 
-  detect = strstr(voice_str, _red);
+  printf("Said: %s\n", voice_str);
+
+  detect = strstr(voice_str, c_lights);
   if (detect != NULL) {
-    broadcastString(testType, _red);
+    broadcastString("0", "0");
     return 0;
   }
   
-  detect = strstr(voice_str, _green);
+  
+  detect = strstr(voice_str, c_take);
   if (detect != NULL) {
-    broadcastString(testType, _green);
+    broadcastString("1", "0");
     return 0;
   }
+
+  detect = strstr(voice_str, c_picture);
+  if (detect != NULL) {
+    broadcastString("1", "1");
+    return 0;
+  }
+  /*
 
   detect = strstr(voice_str, _blue);
   if (detect != NULL) {
@@ -34,7 +50,8 @@ int commandDetect(char const* voice_str)
     broadcastString(testType, _yellow);
     return 0;
   }
+  */
   
-  printf("NOT FOUND\n");
+  printf("No command listed found\n");
   return -1;
 }
