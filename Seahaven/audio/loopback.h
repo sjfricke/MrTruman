4a3c20773327ed5198ca054150abea30e763f24f
Max <mbstrange@wisc.edu>
2017-11-14 01:07:21 -0600

    sourced some fft code, have sample data - just need to run

--- Seahaven/audio/loopback.h
+++ Seahaven/audio/loopback.h
@@ -21,3 +21,14 @@ void loopbackSetup();
 
 // Really wont ever call this
 void loopbackTerminate();
+
+void fftTest();
+
+
+#define SIN_2PI_16 0.38268343236508978
+#define SIN_4PI_16 0.707106781186547460
+#define SIN_6PI_16 0.923879532511286740
+#define C_P_S_2PI_16 1.30656296487637660
+#define C_M_S_2PI_16 0.54119610014619690
+#define C_P_S_6PI_16 1.3065629648763766
+#define C_M_S_6PI_16 -0.54119610014619690
